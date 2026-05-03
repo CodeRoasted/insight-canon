b322eb9b4285dfeea823087f41551fdb7a44a7c2
Emmanuel Prunet <emmanuel.prunet@coderoast.fr>
2026-05-03 18:47:35 +0200

    Fix canon version

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain
 
 class InsightCanonConan(ConanFile):
     name = "insight_canon"
-    version = "0.1.0"
+    version = "1.2.8"
     package_type = "library"
     license = "Apache-2.0"
     url = "https://github.com/coderoast-dev/insight-canon"
